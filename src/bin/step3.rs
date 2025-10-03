b707226414074ead18f83675c637eee514097883
t9a <t9a.dev@icloud.com>
2025-10-03 16:36:56 +0900

    solve: 1.-Two-Sum

--- src/bin/step3.rs
+++ src/bin/step3.rs
@@ -8,14 +8,30 @@
 // レビューを受ける
 // 作れないデータ構造があった場合は別途自作すること
 
+// MEMO
+// step2で他の解答例を見たときにハッシュテーブルを利用したワンパスアルゴリズムを見つけたのでこれで実装した。
+// nums_hash_map.removeとしているのは、戻り値のvalueの参照ではなく所有権を得るため。
+// nums_hash_map.getとすると戻り値で参照が帰ってきて、try_into()するためにclone()する必要があり、払う必要のないコストがかかる。
+// 今回の実装ではペアのnumを見つけた時点でreturnするのでremoveしても以降のコードに影響が無い。
+
+use std::collections::HashMap;
+
 /*
-  時間計算量:
-  空間計算量:
+  時間計算量: O(n)
+  空間計算量: O(n)
 */
 pub struct Solution {}
 impl Solution {
-    pub fn add(left: u64, right: u64) -> u64 {
-        left + right
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut nums_hash_map: HashMap<i32, usize> = HashMap::new();
+        for (i, num) in nums.into_iter().enumerate() {
+            let complement = target - num;
+            if let Some(complement_index) = nums_hash_map.remove(&complement) {
+                return vec![complement_index.try_into().unwrap(), i.try_into().unwrap()];
+            }
+            nums_hash_map.insert(num, i);
+        }
+        vec![]
     }
 }
 
@@ -24,8 +40,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = Solution::add(2, 2);
-        assert_eq!(result, 4);
+    fn step3_test() {
+        let result = Solution::two_sum(vec![2, 7, 11, 15], 9);
+        assert_eq!(result, vec![0, 1]);
+
+        let result = Solution::two_sum(vec![3, 2, 4], 6);
+        assert_eq!(result, vec![1, 2]);
+
+        let result = Solution::two_sum(vec![3, 3], 6);
+        assert_eq!(result, vec![0, 1]);
     }
 }
