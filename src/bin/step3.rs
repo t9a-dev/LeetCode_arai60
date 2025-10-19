ce3b7af5af184575308446728e77b43ad111ef05
t9a <t9a.dev@icloud.com>
2025-10-19 16:20:53 +0900

    solve: 83.Remove Duplicates from Sorted List

--- src/bin/step3.rs
+++ src/bin/step3.rs
@@ -9,23 +9,97 @@
 // 作れないデータ構造があった場合は別途自作すること
 
 /*
-  時間計算量:
-  空間計算量:
+  N=入力全体のサイズ
+  時間計算量: O(N) 入力のノードを全走査するので
+  空間計算量: O(1) インプレイスで入力をそのまま変更して返すので
 */
 
 /*
- 1回目: 分秒
- 2回目: 分秒
- 3回目: 分秒
+ 1回目: 3分14秒
+ 2回目: 4分17秒
+ 3回目: 2分33秒
 */
 
+pub struct ListNode {
+    val: i32,
+    next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    pub fn new(val: i32) -> Self {
+        Self { next: None, val }
+    }
+}
+
 pub struct Solution {}
-impl Solution {}
+impl Solution {
+    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut head = head;
+        let mut current = head.as_mut();
+
+        while let Some(node) = current {
+            while let Some(next_node) = node.next.as_mut() {
+                if node.val != next_node.val {
+                    break;
+                }
+                node.next = next_node.next.take();
+            }
+            current = node.next.as_mut();
+        }
+
+        head
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn vec_to_list_node(values: &Vec<i32>) -> Option<Box<ListNode>> {
+        values.into_iter().rev().fold(None, |child, v| {
+            let mut parent = Box::new(ListNode::new(*v));
+            parent.next = child;
+            Some(parent)
+        })
+    }
+
+    fn list_node_to_vec(head: &Option<Box<ListNode>>) -> Vec<i32> {
+        let mut out = Vec::new();
+        let mut next_node = head;
+
+        while let Some(node) = next_node {
+            out.push(node.val);
+
+            next_node = &node.next;
+        }
+
+        out
+    }
+
     #[test]
-    fn step3_test() {}
+    fn step3_test() {
+        let source_vec = vec![1, 1, 2];
+        let head = vec_to_list_node(&source_vec);
+        assert_eq!(list_node_to_vec(&head), source_vec);
+        assert_eq!(
+            list_node_to_vec(&Solution::delete_duplicates(head)),
+            vec![1, 2]
+        );
+
+        let source_vec = vec![1, 1, 2, 3, 3];
+        let head = vec_to_list_node(&source_vec);
+        assert_eq!(list_node_to_vec(&head), source_vec);
+        assert_eq!(
+            list_node_to_vec(&Solution::delete_duplicates(head)),
+            vec![1, 2, 3]
+        );
+
+        let source_vec = vec![1, 1, 1];
+        let head = vec_to_list_node(&source_vec);
+        assert_eq!(list_node_to_vec(&head), source_vec);
+        assert_eq!(
+            list_node_to_vec(&Solution::delete_duplicates(head)),
+            vec![1]
+        );
+    }
 }
