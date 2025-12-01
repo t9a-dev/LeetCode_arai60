f9fc0ed20816c6ebef1f6d2327d8dd13c648f16c
t9a <t9a.dev@icloud.com>
2025-12-01 11:22:58 +0900

    solve: 121.Best Time to Buy and Sell Stock

--- src/bin/step1.rs
+++ src/bin/step1.rs
@@ -8,26 +8,79 @@
 // 正解したら終わり
 
 /*
+  問題の理解
+  - 株価を表す配列pricesが与えられる。prices[i]はある日の株価である。
+  - ある日に株を買った場合に、最大になる利益を値として返す。
+  - 利益が発生しない場合は0を返す。
+  e.g.
+  prices = [1, 2, 3, 4] out = (prices[3] - prices[0]) = 3
+  prices = [4, 3, 2, 1] out = 0 // どの日に株を購入しても利益が発生しない
+
   何がわからなかったか
-  -
+  - 手作業でやることを考えたときに、
+    - 最小株価を見つける。
+    - 最大株価を見つける。
+    - これらの株価の差を見る。ただし、最小株価の位置iは最大株価のiよりも小さい必要がある。
+    この解法をより単純な考え方にする方法がわからなかった。
 
   何を考えて解いていたか
-  -
+  - 2重ループで組み合わせを見れば解けると思う。ある時点の株価よりも高い株価の時の利益を都度更新していき、最終的に最大の利益を返す。
+  計算量はO(N ^ 2)となる。入力の制約からNは10 ^ 5なので、10 000 000 000回計算することになる。筋が悪そう。もう少し良さそうな方法を考えて思いつかなければこのナイーブな実装をする。
+  - pricesの中で最小を探す。indexをdayとする。線形探索なのでO(N)
+    pricesの中で最大を探す。indexをdayとする。線形探索なのでO(N)
+    min_price_day < max_price_day であれば max_price - min_price を答えとして返す。
+    条件を満たさなければ0を返す。
+    この方法だと利益自体は発生するケースを取りこぼす。
+  時間切れなのでナイーブな実装だけする。
+  Time Limit Exceeded となる実装が出来上がった。
+  解答を見る。
 
-  想定ユースケース
-  -
-
-  正解してから気づいたこと
-  -
+  解法の理解
+  https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/4868897/most-optimized-kadanes-algorithm-java-c-2yt85/
+  - pricesを先頭から全走査しながら、buyに見つけたprices[i]の最小値を入れている。
+  - prices[i] - buy で利益を求めて、利益が今までに見つけた利益よりも大きければ利益を更新している。
+  解法は理解できので、step1a.rsで実装する。
 */
 
 pub struct Solution {}
-impl Solution {}
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        /*
+          このコードはLeetCode採点システム上で Time Limit Exceeded となるコードです。
+        */
+        if prices.is_empty() {
+            return 0;
+        }
+
+        let mut max_profit = 0;
+        for i in 0..prices.len() {
+            for j in i..prices.len() {
+                if prices[j] < prices[i] {
+                    continue;
+                }
+
+                max_profit = max_profit.max(prices[j] - prices[i]);
+            }
+        }
+
+        max_profit
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn step1_test() {}
+    fn step1_test() {
+        assert_eq!(Solution::max_profit(vec![7, 1, 5, 3, 6, 4]), 5);
+        assert_eq!(Solution::max_profit(vec![7, 2, 5, 3, 6, 1]), 4);
+        assert_eq!(Solution::max_profit(vec![1, 2, 3, 4]), 3);
+        assert_eq!(Solution::max_profit(vec![4, 3, 2, 1]), 0);
+
+        assert_eq!(Solution::max_profit(vec![2, 1]), 0);
+        assert_eq!(Solution::max_profit(vec![1, 2]), 1);
+        assert_eq!(Solution::max_profit(vec![1]), 0);
+        assert_eq!(Solution::max_profit(vec![]), 0); // 株を購入できないので利益は0で意味が通ると思う
+    }
 }
--- src/bin/step2a.rs
+++ src/bin/step2a.rs
@@ -1,23 +1,27 @@
-// Step3
-// 目的: 覚えられないのは、なんか素直じゃないはずなので、そこを探し、ゴールに到達する
+// Step2a
+// 目的: 一次元DPによる実装方法を練習しておく。
 
 // 方法
-// 時間を測りながらもう一度解く
-// 10分以内に一度もエラーを吐かず正解
-// これを3回連続でできたら終わり
-// レビューを受ける
-// 作れないデータ構造があった場合は別途自作すること
+// Step1のコードを読みやすくしてみる
+// 他の人のコードを2つは読んでみること
+// 正解したら終わり
+
+// 以下をメモに残すこと
+// 講師陣はどのようなコメントを残すだろうか？
+// 他の人のコードを読んで考えたこと
+// 改善する時に考えたこと
 
 /*
+  解法の考え方
+  - pricesを先頭から全走査しつつ、最小の価格を更新する
+  - 今見ている株価prices[i]と最小の価格差を計算してmax_profits配列に入れていく
+  - 最後にmax_profits配列の中から最大の値を返す
+
   n = prices.len()
   時間計算量: O(n)
   空間計算量: O(n)
-*/
 
-/*
-  1回目: 1分24秒
-  2回目: 1分38秒
-  3回目: 1分14秒
+  max_profitsを配列で管理せず、変数で管理すると空間計算量がO(1)となる。
 */
 
 pub struct Solution {}
@@ -27,14 +31,14 @@ impl Solution {
             0 => return 0,
             _ => prices[0],
         };
-        let mut max_price = 0;
+        let mut max_profits = vec![0];
 
         for price in &prices[1..] {
             min_price = min_price.min(*price);
-            max_price = max_price.max(*price - min_price);
+            max_profits.push(*price - min_price);
         }
 
-        max_price
+        max_profits.into_iter().max().unwrap_or(0)
     }
 }
 
@@ -43,7 +47,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn step3_test() {
+    fn step2a_test() {
         assert_eq!(Solution::max_profit(vec![7, 1, 5, 3, 6, 4]), 5);
         assert_eq!(Solution::max_profit(vec![7, 2, 5, 3, 6, 1]), 4);
         assert_eq!(Solution::max_profit(vec![1, 2, 3, 4]), 3);
